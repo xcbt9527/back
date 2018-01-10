708944faf49a108594aa6ebb6c7dafe7557d38c9
xcbt9527 <admin520>
2018-01-10 20:34:37 +0800

    临时提交

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+var koa = require('koa');
+var router = koa.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
