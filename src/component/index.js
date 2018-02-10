bcf349289e22ee6b9708f26bceb8860e164e3f1d
xcbt9527 <admin520>
2018-02-10 13:04:13 +0800

    新增table

--- src/component/index.js
+++ src/component/index.js
@@ -7,6 +7,8 @@ import vue from 'vue';
 //组件
 import update from "./update/index.ts";
 import canvas from "./canvas/canvas.ts";
+import mtable from "./table/table.ts";
 //引入组件
 vue.component('m-update', update);
 vue.component('m-canvas', canvas);
+vue.component('m-table', mtable);
--- src/component/table/table.ts
+++ src/component/table/table.ts
@@ -10,7 +10,7 @@
 
 import Vue from 'vue';
 import { Component, Watch, Prop } from 'vue-property-decorator';
-import htmltepl from "./canvas.html";
+import htmltepl from "./table.html";
 @Component({
     template: htmltepl,
     name: 'm-table',
@@ -20,10 +20,36 @@ import htmltepl from "./canvas.html";
 export default class updatets extends Vue {
 
     @Prop({ default: [] }) value: Array<any>;   //v-model的值
+    currentPage:number = 1;
+    pagesize:number = 25;
+    get total(){
+        return this.value.length;
+    }
+    get userarr(){
+        console.log(this.value.slice(this.pagesize*(this.currentPage -1),this.pagesize*this.currentPage))
+        return this.value.slice(this.pagesize*(this.currentPage -1),this.pagesize*this.currentPage);
+    }
     mounted() {
         this.init();
     }
     init() {
 
+    }
+    add(){
+        this.$emit('add');
+    }
+    /**
+     * 获取分页大小
+     * @param row 
+     */
+    handleSizeChange(row){
+        console.log(row);
+    }
+    /**
+     * 获取分页
+     * @param row 
+     */
+    handleCurrentChange(row){
+        console.log(row);
     }
 }
